f050c78d8367b3d0cc07e90ee8c6276ff63d4b77
magolaliet2022 <cholmarial1@gmail.com>
2022-06-09 22:29:42 -0700

    commit for task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
-#include "main.h"
+include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit - checks if parameter is a number between 0 to 9.
- * @c: input number.
- * Return: 1 if is a number (0 to 9), 0 in other case.
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-int _isdigit(int c)
+int main(void)
 {
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
