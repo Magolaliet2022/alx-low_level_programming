65cafc0cb73fe28ee363b88b59d0111b2414a9aa
magolaliet2022 <cholmarial1@gmail.com>
2022-06-09 20:53:51 -0700

    commit for task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,8 @@
 /**
  * _isuper - evaluate if letter is uppercase .
  * @c: print int
- * Return always 0.
- */
+ * Return always 0 .
+*/
 
 int _isupper(int c)
 {
@@ -17,4 +17,5 @@ else
 {
 return (0);
 }
+
 }
