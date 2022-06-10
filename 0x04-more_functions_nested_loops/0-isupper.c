4d338434e36ae4dfe272612c9ebf2fb75b35ad02
magolaliet2022 <cholmarial1@gmail.com>
2022-06-09 22:56:34 -0700

    commit for task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
+ *  * _isupper - evaluate if letter is uppercase.
+ *   *@c: print int
+ *    * Return: Always 0.
+ *     */
+int _isupper(int c)
 {
-    char c;
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+	}
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	else
+	{return (0);
+	}
+	 
 }
