f7bf6491a17f1ced881d0ee1d3f577d95f7a78dd
magolaliet2022 <cholmarial1@gmail.com>
2022-06-12 22:56:53 -0700

    commit for task one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (success/correct)
  */
 
@@ -22,11 +22,11 @@ printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
 
 else if (n % 10 == 0)
 {
-printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
+printf("last digit of %i is %i and is 0\n", n, n % 10);
 }
 else
 {
-printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
+printf("last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 
 return (0);
