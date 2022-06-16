614a5d525926767d38b09a61f50faf504d14c05c
Magolaliet2022 <cholmarial1@gmail.com>
2022-06-16 02:56:04 -0700

    commit for task three of alx project 0x06

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncpy - compares two strings
+ *_strcmp - compares two strings
  *@s1: A pointer to a character that will be changed
  *@s2: A pointer to a character that will be changed
  * Return: dest
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 char *str_one = s1;
 char *str_two = s2;
 
-while (*str_one != '\0' && *str_two !='\0' && *str_one == *str_two)
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 {
 str_one++;
 str_two++;
