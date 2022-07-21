6f3bef2806527c6a8fff3156478d688d4082669c
Magolaliet2022 <cholmarial1@gmail.com>
2022-07-20 23:15:15 -0700

    commit for task three

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-
 	if (!new)
 		return (NULL);
 
@@ -26,6 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			*head = new;
 			return (new);
 		}
+
 	while (temp->next)
 		temp = temp->next;
 
