5ba21b3a0643a2f0c6abf67b353ae522c71bdaec
Magolaliet2022 <cholmarial1@gmail.com>
2022-07-29 06:56:36 -0700

    commit for task zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, fread, fwrite
+	ssize_t file, fread, fwrite;
 		char *totalSize;
 
 	totalSize = malloc(sizeof(char) * letters);
